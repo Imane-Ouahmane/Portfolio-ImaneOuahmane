e03bcbde9147a0fc1ea04f3327d06ded6e337619
Imane-Ouahmane <imaneouahmane202@gmail.com>
2023-12-18 15:01:58 +0100

    Socialmedia links

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { FaGithub,FaLinkedin,  } from 'react-icons/fa';
+import {HiOutlineMail} from 'react-icons/hi';
+import { BsFillPersonFill } from 'react-icons/bs'; // Correct import
+
+
+function SocialLinks() {
+
+    const links = [
+        {id:1,
+        child:(
+            <>
+                LinkedIn <FaLinkedin size={30}/>
+            </>
+        ),
+        href:'https://www.linkedin.com/in/imane-ouahmane/',
+        style:'rounded-tr-md'
+    },
+    {id:2,
+        child:(
+            <>
+                GitHub <FaGithub size={30}/>
+            </>
+        ),
+        href:'https://github.com/Imane-Ouahmane',
+        
+    },
+    {id:3,
+        child:(
+            <>
+                Mail <HiOutlineMail size={30}/>
+            </>
+        ),
+        href:'mailto:imaneouahmane202@gmail.com',
+        
+    },
+    {id:4,
+        child:(
+            <>
+                Resume <BsFillPersonFill size={30}/>
+            </>
+        ),
+        href:'/public/CV-Imane Ouahmane-EN.pdf',
+        style:'rounded-br-md',
+        download:true,
+    },
+    ]
+  return (
+    <div className='hidden lg:flex flex-col top-[35%] left-0 fixed'>
+        <ul>
+            {links.map(({id, child, href, style, download}) => (
+
+                <li key={id} className='flex justify-between bg-gray-500 items-center w-40 h-14 p-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300'>
+                <a 
+                    href={href} 
+                    className='flex  text-white justify-between items-center w-full '
+                    download={download}
+                    target='_blank'
+                    rel='norereferrer'
+                >
+                    {child}
+                </a>
+                </li>
+
+            ))}
+           
+        </ul>
+    </div>
+  )
+}
+
+export default SocialLinks
