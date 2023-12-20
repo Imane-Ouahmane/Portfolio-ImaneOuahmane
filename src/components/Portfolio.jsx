63cfecf1781fae759bf15bb31da8001d43118099
Imane-Ouahmane <imaneouahmane202@gmail.com>
2023-12-20 17:24:13 +0100

    change the image

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import portfolio from '../assets/portfolio/portfolioimg.jpg';
+import portfolio from '../assets/portfolio/portfoimg.jpg';
 
 function Portfolio() {
   const portfolios = [
