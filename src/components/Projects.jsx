461f12e241bc79dcfa282e75348eed18d2db4d5c
Imane-Ouahmane <imaneouahmane202@gmail.com>
2024-01-01 16:31:19 +0100

    make the demo button work

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,14 @@
+import { useState } from 'react';
 import React from 'react';
+import Popup from './Popup'; 
 import portfolio from '../assets/portfolio/portfolioimg.jpg';
+import homeport from '../assets/portfolio/homeport.jpg';
+import aboutport from '../assets/portfolio/aboutport.jpg';
+import projectsport from '../assets/portfolio/projectsport.jpg';
+import skillsport from '../assets/portfolio/skillsport.jpg';
+import contactport from '../assets/portfolio/contactport.jpg';
+
+
 
 function Projects() {
   const portfolios = [
@@ -15,6 +24,17 @@ function Projects() {
     window.location.href = githubLink;
   };
 
+  const [showPopup, setShowPopup] = useState(false);
+
+  const openPopup = () => {
+    setShowPopup(true);
+  };
+
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+
+
   return (
     <div name='projects' className='bg-white w-full text-black md:h-screen'>
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
@@ -28,7 +48,10 @@ function Projects() {
             <div key={id} className='shadow-md shadow-gray-600 bg-black rounded-lg hover:scale-105'>
               <img src={src} alt="" className='rounded-md w-full h-auto' />
               <div className='flex items-center justify-center'>
-                <button className='w-1/2 px-6 py-3 m-4 hover:scale-105 bg-gray-800 text-white rounded-md'>
+                <button
+                  className='w-1/2 px-6 py-3 m-4 hover:scale-105 bg-gray-800 text-white rounded-md'
+                  onClick={openPopup}
+                >
                   Demo
                 </button>
                 <button
@@ -41,6 +64,12 @@ function Projects() {
             </div>
           ))}
         </div>
+        {showPopup && (
+          <Popup
+            images={[ homeport, aboutport, projectsport, skillsport, contactport]}
+            onClose={closePopup}
+          />
+        )}
       </div>
     </div>
   );
