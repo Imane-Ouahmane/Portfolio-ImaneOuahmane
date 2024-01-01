ca5d699c7c6676b1a3e4f9936dfebf30a7b84cf3
Imane-Ouahmane <imaneouahmane202@gmail.com>
2024-01-01 12:31:01 +0100

    make tha code button works

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,13 +6,17 @@ function Projects() {
     {
       id: 1,
       src: portfolio,
+      link: 'https://github.com/Imane-Ouahmane/Portfolio-ImaneOuahmane/tree/main',
     },
-
     // Add more projects here
   ];
 
+  const redirectToGitHub = (githubLink) => {
+    window.location.href = githubLink;
+  };
+
   return (
-     <div name='projects' className='bg-white w-full text-black md:h-screen'>
+    <div name='projects' className='bg-white w-full text-black md:h-screen'>
       <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
         <div className='pd-8'>
           <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Projects</p>
@@ -20,14 +24,17 @@ function Projects() {
         </div>
 
         <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-          {portfolios.map(({ id, src }) => (
+          {portfolios.map(({ id, src, link }) => (
             <div key={id} className='shadow-md shadow-gray-600 bg-black rounded-lg hover:scale-105'>
               <img src={src} alt="" className='rounded-md w-full h-auto' />
               <div className='flex items-center justify-center'>
                 <button className='w-1/2 px-6 py-3 m-4 hover:scale-105 bg-gray-800 text-white rounded-md'>
                   Demo
                 </button>
-                <button className='w-1/2 px-6 py-3 m-4 hover:scale-105 bg-gray-800 text-white rounded-md'>
+                <button
+                  onClick={() => redirectToGitHub(link)}
+                  className='w-1/2 px-6 py-3 m-4 hover:scale-105 bg-gray-800 text-white rounded-md'
+                >
                   Code
                 </button>
               </div>
